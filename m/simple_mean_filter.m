5d58406911f276ff7b39092a5efa65e9ecc7b865
quanwei <quanwei9958@126.com>
2016-11-04 23:29:44 +0800

    add filter

---
+++ m/simple_mean_filter.m
@@ -0,0 +1,9 @@
+% 均值滤波器
+
+function img = simple_mean_filter(img)
+  kernel = 3; %核函数大小
+
+  assert(mod(kernel,2)==1);  
+  kernelMatrix = ones(kernel,kernel);
+  img = imfilter(img,kernelMatrix);
+end
